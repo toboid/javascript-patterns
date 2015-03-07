d4f2c4ebc3db2633da9780dd5a4c37ab39f243ed
toby <tobytronics@gmail.com>
2015-03-07 16:49:40 +0000

    Forward to own

---
+++ mixins-delegation-forwarding/forward-to-own.js
@@ -0,0 +1,56 @@
+var expect = require('chai').expect;
+
+function forwardToOwn (receiver, methods, propertyName) {
+  methods.forEach(function (methodName) {
+    receiver[methodName] = function () {
+      var toProvider = receiver[propertyName];
+      return toProvider[methodName].apply(toProvider, arguments);
+    };
+  });
+
+  return receiver;
+};
+
+describe('forward to own', function () {
+  var customer123, 
+      accountHolder;
+
+  beforeEach(function () {
+    customer123 = {};
+
+    accountHolder = {
+      _balance: 10,
+      calculateInterest: function() {
+        return this._balance * 0.05;
+      }
+    };
+
+    customer123.account = accountHolder;
+
+    forwardToOwn(customer123, ['calculateInterest'], 'account');
+  });
+
+  it('adds methods to receiver', function() {
+    expect(customer123.calculateInterest()).to.be.equal(0.5);
+  });
+
+  it('no state added to receiver', function() {
+    expect(customer123._balance).to.be.undefined;
+  });
+
+  it('state is held by the provider', function() {
+    expect(accountHolder._balance).to.be.equal(10);
+  });   
+
+  it('can change implementation at run time', function() {
+    // State changes too since it's held on the provider
+    var alternativeAccount = {
+      _balance: 5,
+      calculateInterest: function() {
+        return this._balance * 0.10;
+      }
+    };
+    customer123.account = alternativeAccount;
+    expect(customer123.calculateInterest()).to.be.equal(0.5);
+  });
+});
